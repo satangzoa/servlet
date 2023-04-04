1f115985b1b59606bb05b7412ad41f696e24c080
A <A@DESKTOP-NKSGQVR>
2023-04-04 10:33:19 +0900

    servlet 시작

---
+++ src/main/java/com/oraclejava/project/web/MyServlet.java
@@ -0,0 +1,47 @@
+package com.oraclejava.project.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out =  response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("이름: 박소망 <br><br>");
+		out.println("키: 크다 cm  <br><br>");
+		out.println("몸무게: 비밀임 kg");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
